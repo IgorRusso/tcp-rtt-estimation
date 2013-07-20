2cd45d5c7203d6d31d4b2ff9c5c6002da4214d9a
IgorRusso <igor.souza.russo@gmail.com>
2013-07-20 12:11:45 -0300

    Criação dos arquivos
    Captura ok

---
+++ captura.py
@@ -0,0 +1,37 @@
+import csv
+from subprocess import Popen, PIPE
+import re
+import time
+import array
+
+hostname = "www.ufjf.br"
+
+RTT = array.array('f')
+
+def run(host):
+
+    p = proc=Popen(['ping',host,'-n', '1','-w','10000'], stdout=PIPE)
+    ret =  p.stdout.read()
+    m = re.search('tempo=(.*)ms', ret)
+  
+    if m:
+        rttMedido = float(m.group(1))
+        print rttMedido
+        RTT.append(rttMedido)        
+
+    else: 
+        print 'Timeout'        
+
+for i in range(0,500):
+    run(hostname)
+    time.sleep(0.4)
+    print 'Ping',i
+
+fileName = 'rttSamples.csv'
+ofile  = open(fileName, "wb")
+writer = csv.writer(ofile)
+
+for row in zip(RTT):    
+    writer.writerow(row)
+
+ofile.close()
